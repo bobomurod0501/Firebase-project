05b7e7fc1d16003bdc4ae52dbedf1386fa886355
Bobomurod <bobomurodeshmahmatov@gmail.com>
2025-09-02 12:27:24 +0500

    fixed some errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom'
-import styles from "../components/Navbar.module.css"
+import styles from "./Navbar.module.css"
 import { useGlobalContexts } from '../hooks/useGlobalContexts';
 import { useSignout } from '../hooks/useSignout';
 
